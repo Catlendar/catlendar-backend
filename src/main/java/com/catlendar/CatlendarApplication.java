3e6ffa93c3b7815ba672f55b6084e0a37af3dc03
jiwanchoi <wldhks303@naver.com>
2024-01-17 02:37:37 +0900

    initial commit

---
+++ src/main/java/com/catlendar/CatlendarApplication.java
@@ -0,0 +1,14 @@
+package com.catlendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatlendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatlendarApplication.class, args);
+		System.out.println("첫번째 커밋");
+	}
+
+}
---
+++ src/main/java/com/catlendar/controller/BoardController.java
@@ -0,0 +1,4 @@
+package com.catlendar.controller;
+
+public class BoardController {
+}
