e3bed26497e75a94fb65f21a65f122a2c3114d15
jiwanchoi <wldhks303@naver.com>
2024-01-22 00:12:59 +0900

    불필요 코드정리, compile time 기록

--- src/main/java/com/catlendar/CatlendarApplication.java
+++ src/main/java/com/catlendar/CatlendarApplication.java
@@ -2,10 +2,16 @@ package com.catlendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 public class CatlendarApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CatlendarApplication.class, args);
-		System.out.println("comfile success");
+		String dateTime = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm:ss a")
+				.format(LocalDateTime.now());
+		System.out.println("successfully compiled the code. "+dateTime);
 	}
 }
