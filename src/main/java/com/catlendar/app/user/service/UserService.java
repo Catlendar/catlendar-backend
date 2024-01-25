9281af2383ff76db538593021c328be75695dc6e
jiwanchoi <wldhks303@naver.com>
2024-01-25 21:28:29 +0900

    dto 변경

--- src/main/java/com/catlendar/app/user/service/UserService.java
+++ src/main/java/com/catlendar/app/user/service/UserService.java
@@ -1,6 +1,6 @@
 package com.catlendar.app.user.service;
 import com.catlendar.app.user.mapper.UserMapper;
-import com.catlendar.app.user.model.User;
+import com.catlendar.app.user.model.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,17 +13,17 @@ public class UserService {
     @Autowired
     private final UserMapper userMapper;
     @Transactional
-    public List<User> getUserList(){
-        List<User> userInfo = userMapper.getUserList();
+    public List<UserInfo> getUserList(){
+        List<UserInfo> userInfo = userMapper.getUserList();
         return userInfo;
     }
     @Transactional
-    public User getUser(User user){
-        User userInfo = userMapper.getUser(user);
+    public UserInfo getUser(UserInfo user){
+        UserInfo userInfo = userMapper.getUser(user);
         return userInfo;
     }
     @Transactional
-    public boolean insertUser(User user){
+    public boolean insertUser(UserInfo user){
         int result = userMapper.insertUser(user);
         if(result<1){
             return false;
@@ -31,7 +31,7 @@ public class UserService {
         return true;
     }
     @Transactional
-    public boolean updateUser(User user){
+    public boolean updateUser(UserInfo user){
         int result = userMapper.updateUser(user);
         if(result<1){
             return false;
@@ -39,7 +39,7 @@ public class UserService {
         return true;
     }
     @Transactional
-    public boolean deleteUser(User user){
+    public boolean deleteUser(UserInfo user){
         int result = userMapper.deleteUser(user);
         if(result<1){
             return false;
