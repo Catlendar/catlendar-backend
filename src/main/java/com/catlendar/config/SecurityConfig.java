7ac6d3a611d4b61826c2aa267d6dfa1046f4f7af
jiwanchoi <wldhks303@naver.com>
2024-02-14 01:13:05 +0900

    fix cors error2

--- src/main/java/com/catlendar/config/SecurityConfig.java
+++ src/main/java/com/catlendar/config/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +24,10 @@ public class SecurityConfig {
     @Value("${jwt.secret}")
     private String secretKey;
 
-    @Bean
+        @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return  httpSecurity
-                .cors().and()
+                .cors().configurationSource(corsConfigurationSource())
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement()
@@ -43,5 +46,43 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return  httpSecurity
+//                .cors().and()
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers(
+//                        "/user/login"		//로그인
+//                        ,"/user/signUp"			        //회원가입
+//                        ,"/user/emailVerify"			//이메일 중복체크
+//                ).permitAll()
+//                .antMatchers(HttpMethod.POST,
+//                        "/*/*"
+//                ).authenticated()
+//                .and()
+//                .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//    }
 
 }
